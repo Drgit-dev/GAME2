0c5a866d9db7fcbcd74f670665140ee3ab8dabdf
Daniel <danireguilon@gmail.com>
2024-12-12 18:30:44 +0100

    Hola

---
+++ src/main/java/org/example/Bow.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Bow {
+    public final int spriteWidth = 64, spriteHeight = 64;
+    private int spriterow;
+    private int spritecol;
+    private static final String PATH_TO_TILE_SPRITESHEET = "";
+}
